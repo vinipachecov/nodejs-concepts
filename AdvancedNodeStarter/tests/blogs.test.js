944013dc6fbc65c8bf6c05ab89ea433704c48812
vinicius p vieira <vinipachecov@gmail.com>
2018-06-09 12:45:44 -0300

    refactor of the error tests.

--- AdvancedNodeStarter/tests/blogs.test.js
+++ AdvancedNodeStarter/tests/blogs.test.js
@@ -68,40 +68,29 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in ', async () => {
-  test('User cannot get a list of posts ', () => {
-    const result = await page.evaluate(
-      () => {
-       return fetch('/api/blogs', {
-          method: 'GET',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json'
-          }          
-        }).then(res => res.json());
-      });              
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs'
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'T',
+        content: 'C'
+      }
+    }
+  ];
+
+  test('Blog related actions are prohibited ', async () => {
+    const results = await page.execRequests(actions);  
+    
+    for (let result of results) {
       expect(result).toEqual({ error: 'You must log in!'});
-  })
-  
+    }            
+  });
 
 
-  test('User cannot create blog posts ',  async () => {
-   const result = await page.evaluate(
-      () => {
-       return  fetch('/api/blogs', {
-          method: 'POST',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            title: ' My Ttitle',
-            content: 'My Content'
-          })
-        }).then(res => res.json());
-      });        
-    console.log(result);
-
-    expect(result).toEqual({ error: 'You must log in!'});
-  });    
 });
 
