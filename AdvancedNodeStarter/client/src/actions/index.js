d433c994bfb6b85381b314d125067ba39c560f52
vinicius p vieira <vinipachecov@gmail.com>
2018-06-14 22:25:34 -0300

    finishing the course

--- AdvancedNodeStarter/client/src/actions/index.js
+++ AdvancedNodeStarter/client/src/actions/index.js
@@ -13,8 +13,23 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitBlog = (values, history) => async dispatch => {
-  const res = await axios.post('/api/blogs', values);
+export const submitBlog = (values, file, history) => async dispatch => {
+
+  // get the aws signed url
+  const uploadConfig  = await axios.get('/api/upload');
+
+  await axios.put(uploadConfig.data.url, file, {
+    headers: {
+      'Content-Type': file.type
+    }
+  });
+
+
+
+  const res = await axios.post('/api/blogs', { 
+    ...values,
+    imageUrl: uploadConfig.data.key
+  });
 
   history.push('/blogs');
   dispatch({ type: FETCH_BLOG, payload: res.data });
